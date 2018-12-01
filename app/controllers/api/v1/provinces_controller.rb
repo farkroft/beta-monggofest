5034b204194da3d3b72eb0f0b107f91556062e44
Angga Febri Eka Susani <angga.febrieka@gmail.com>
2018-12-01 10:18:43 +0700

    [Angga]Index and show API

--- app/controllers/api/v1/provinces_controller.rb
+++ app/controllers/api/v1/provinces_controller.rb
@@ -1,15 +1,47 @@
 class Api::V1::ProvincesController < ApplicationController
-    before_action :set_province, only: [:show, :edit, :update, :destroy]
     protect_from_forgery with: :null_session
  
     def index
         @prov = Province.all
         if @prov.present?
-             render json:  @prov     
-            else
-                render json: {message_error: 'province null'}, status: :unprocessable_entity
+             render json:  @prov, status: :ok     
+        else
+             render json: {message_error: 'province null'}, status: :unprocessable_entity
         end
     end
-    
-end
+    def show
+        @prov = Province.find(params[:id])
+       # @prov = Province.all
+        if @prov.present?
+            render json: @prov, status: :ok
+        else
+            render json: {message_error: 'province null'}, status: :unprocessable_entity
+        end
+    end
 
+    
+    / def create
+        @prov = Province.new(prov_params)
+       if @prov.save
+            render json: {message: 'success to insert', }, status: :created
+        else
+            render json: { message: 'failed inserted'}, status: :unprocessable_entity
+        end
+    end
+    #method to destroy
+    def destroy
+        if @prov
+           @prov.destroy!
+            render json:{ message: 'success deleted province'}, status: :ok
+        else
+            render json: { message:'failed deleted province'}, staus: :unprocessable_entity
+        end
+    end
+    #find the province by id
+    def find_prov
+        @prov = Province.find_by(id: params[:id])
+    end
+    def prov_params
+        params.permit(:name)
+    end */
+end
