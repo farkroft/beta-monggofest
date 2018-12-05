362000dc9d6183ca8e4debac7857ab69a2b51d73
farkroft <fajarar77@gmail.com>
2018-12-05 14:30:31 +0700

    added base model for UserInvestor, reference to User but not reference with product_id yet because the table didnt exist

---
+++ app/models/user_investor.rb
@@ -0,0 +1,2 @@
+class UserInvestor < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,65 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_25_023908) do
+ActiveRecord::Schema.define(version: 2018_12_05_072620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "product_invests", force: :cascade do |t|
+    t.bigint "product_id"
+    t.float "price"
+    t.integer "slot"
+    t.integer "count_view"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_product_invests_on_product_id"
+  end
+
+  create_table "product_types", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.bigint "province_id"
+    t.bigint "product_type_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_type_id"], name: "index_products_on_product_type_id"
+    t.index ["province_id"], name: "index_products_on_province_id"
+  end
+
+  create_table "provinces", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "sliders", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.string "url"
+    t.integer "sort_number"
+    t.boolean "isActive", default: true
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_investors", force: :cascade do |t|
+    t.bigint "user_investors_id"
+    t.bigint "user_id"
+    t.integer "investor_slot"
+    t.float "investor_pay"
+    t.integer "invest_year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_user_investors_on_user_id"
+    t.index ["user_investors_id"], name: "index_user_investors_on_user_investors_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
@@ -28,4 +83,7 @@ ActiveRecord::Schema.define(version: 2018_11_25_023908) do
     t.index ["email"], name: "index_users_on_email"
   end
 
+  add_foreign_key "product_invests", "products"
+  add_foreign_key "products", "product_types"
+  add_foreign_key "products", "provinces"
 end
