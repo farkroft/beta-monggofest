17ea37392dae25154e4081b79c5e95b9885bcb54
farkroft <fajarar77@gmail.com>
2018-12-10 12:20:36 +0700

    linter

--- app/controllers/api/v1/product_invests_controller.rb
+++ app/controllers/api/v1/product_invests_controller.rb
@@ -1,42 +1,42 @@
 class Api::V1::ProductInvestsController < ApplicationController
-	#before_action :authenticate_user
-	before_action :find_prodinvest, only: [:destroy, :show]
+  # before_action :authenticate_user
+  before_action :find_prodinvest, only: %i[destroy show]
 
-def index
-	prodinvests = ProductInvest.all
-	if prodinvests.present?
-		render json: {
-			status: 'OK', results: prodinvests.as_json(include: [:product, :kecamatan]), 
-			error: nil
-		}, status: :ok
-	else
-		render json: {
-			status: 'FAIL', results: nil, error: 'Data is Empty'
-		}, status: :unprocessable_entity
-	end
-end
+  def index
+    prodinvests = ProductInvest.all
+    if prodinvests.present?
+      render json: {
+        status: 'OK', results: prodinvests.as_json(include: %i[product kecamatan]),
+        error: nil
+      }, status: :ok
+    else
+      render json: {
+        status: 'FAIL', results: nil, error: 'Data is Empty'
+      }, status: :unprocessable_entity
+    end
+  end
 
-	def show
-		if @prodinvest.present?
-			@prodinvest.count_view += 1
-			@prodinvest.save
-			render json: {
-				status: 'OK', results: @prodinvest, error: nil
-			}, status: :ok
-		else
-			render json: {
-				status: 'FAIL', results: nil, error: 'Data not found'
-			}, status: :unprocessable_entity
-		end
-	end
+  def show
+    if @prodinvest.present?
+      @prodinvest.count_view += 1
+      @prodinvest.save
+      render json: {
+        status: 'OK', results: @prodinvest, error: nil
+      }, status: :ok
+    else
+      render json: {
+        status: 'FAIL', results: nil, error: 'Data not found'
+      }, status: :unprocessable_entity
+    end
+  end
 
-	private
+  private
 
-	def find_prodinvest
-		@prodinvest = ProductInvest.find_by(id: params[:id])
-	end
+  def find_prodinvest
+    @prodinvest = ProductInvest.find_by(id: params[:id])
+  end
 
-	def prodinvest_params
-		params.require(:prodinvest).permit(:price, :slot, :count_view)
-	end
+  def prodinvest_params
+    params.require(:prodinvest).permit(:price, :slot, :count_view)
+  end
 end
