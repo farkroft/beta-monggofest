1111de8f7acb34d72a3fe523774294b4a469663c
Angga Febri Eka Susani <angga.febrieka@gmail.com>
2018-12-13 17:12:26 +0700

    [Angga]Resynch develop

--- app/models/province.rb
+++ app/models/province.rb
@@ -1,9 +1,4 @@
 class Province < ApplicationRecord
-<<<<<<< HEAD
   has_many :regionals, dependent: false
-  has_many :kecamatans, through: :regionals
-=======
-    has_many :regionals
-    has_many :kecamatans, through: :regional
->>>>>>> 815941e9e3f23d1bac59a9006bab048686cead55
+  has_many :subdistcricts, through: :regionals
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_10_064716) do
+ActiveRecord::Schema.define(version: 2018_12_13_090941) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "pictures", force: :cascade do |t|
-    t.bigint "product_id"
-    t.string "photo"
-  end
   create_table "payment_details", force: :cascade do |t|
     t.bigint "user_investor_id"
     t.integer "payment_method"
@@ -31,16 +27,12 @@ ActiveRecord::Schema.define(version: 2018_12_10_064716) do
     t.index ["user_investor_id"], name: "index_payment_details_on_user_investor_id"
   end
 
-  create_table "product_invest_details", force: :cascade do |t|
-    t.bigint "product_invest_id"
-    t.text "description"
-    t.integer "period"
-    t.integer "return_value"
-    t.integer "share_periode"
-    t.text "background"
+  create_table "pictures", force: :cascade do |t|
+    t.bigint "product_id"
+    t.string "photo"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["product_invest_id"], name: "index_product_invest_details_on_product_invest_id"
+    t.index ["product_id"], name: "index_pictures_on_product_id"
   end
 
   create_table "product_invests", force: :cascade do |t|
@@ -53,7 +45,6 @@ ActiveRecord::Schema.define(version: 2018_12_10_064716) do
     t.index ["product_id"], name: "index_product_invests_on_product_id"
   end
 
-  
   create_table "products", force: :cascade do |t|
     t.bigint "subdistricts_id"
     t.integer "product_type", default: 0
@@ -83,17 +74,8 @@ ActiveRecord::Schema.define(version: 2018_12_10_064716) do
     t.string "url"
     t.integer "sort_number"
     t.boolean "isActive", default: true
-  end
-  create_table "user_investors", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "product_invests_id"
-    t.integer "investor_slot"
-    t.float "investor_pay"
-    t.integer "invest_year"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["product_invests_id"], name: "index_user_investors_on_product_invests_id"
-    t.index ["user_id"], name: "index_user_investors_on_user_id"
   end
 
   create_table "subdistricts", force: :cascade do |t|
@@ -107,6 +89,18 @@ ActiveRecord::Schema.define(version: 2018_12_10_064716) do
   create_table "tests", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "user_investors", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "product_invests_id"
+    t.integer "investor_slot"
+    t.float "investor_pay"
+    t.integer "invest_year"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_invests_id"], name: "index_user_investors_on_product_invests_id"
+    t.index ["user_id"], name: "index_user_investors_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -123,15 +117,12 @@ ActiveRecord::Schema.define(version: 2018_12_10_064716) do
     t.index ["email"], name: "index_users_on_email"
   end
 
+  add_foreign_key "payment_details", "user_investors"
   add_foreign_key "pictures", "products"
+  add_foreign_key "product_invests", "products"
   add_foreign_key "products", "subdistricts", column: "subdistricts_id"
   add_foreign_key "regionals", "provinces"
   add_foreign_key "subdistricts", "regionals"
-  add_foreign_key "payment_details", "user_investors"
-  add_foreign_key "product_invest_details", "product_invests"
-  add_foreign_key "product_invests", "products"
-  add_foreign_key "products", "product_types"
-  add_foreign_key "regionals", "provinces"
   add_foreign_key "user_investors", "product_invests", column: "product_invests_id"
   add_foreign_key "user_investors", "users"
 end
