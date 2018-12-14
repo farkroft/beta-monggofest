cf6c9b524369943316fcb178d10f2212356c6807
Angga Febri Eka Susani <angga.febrieka@gmail.com>
2018-12-14 13:49:37 +0700

    [Angga]Resynch develop

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,12 +1,5 @@
 class Product < ApplicationRecord
-<<<<<<< HEAD
   belongs_to :subdistrict
   has_many :pictures, dependent: :destroy
   enum product_type: %i[Sapi Kambing Ayam Bebek Ikan]
-=======
-  belongs_to :kecamatan
-  belongs_to :product_type
-  has_many :product_invests
-  #has_many :regional, through: :kecamatan
->>>>>>> 815941e9e3f23d1bac59a9006bab048686cead55
 end
