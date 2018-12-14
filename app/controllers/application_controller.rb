cc25696472fc2bd62d2afda7b4f80abe8f8559e7
raufhm <abdulraufmustakim@gmail.com>
2018-12-14 16:50:40 +0700

    [rauf] fix application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,3 @@
 # frozen_string_literal: true
-<<<<<<< HEAD:app/controllers/application_controller.rb
-
-=======
->>>>>>> dad156f12f96ba66462724035f893dbf103de8ac:app/controllers/application_controller.rb
 class ApplicationController < ActionController::Base
 end
