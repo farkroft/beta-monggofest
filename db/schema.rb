a32d3bc76f3448c430f5b47c98ca98c44a8db909
Angga Febri Eka Susani <angga.febrieka@gmail.com>
2018-12-14 15:55:51 +0700

    [Angga]perbaikan heroku

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_13_090941) do
+ActiveRecord::Schema.define(version: 2018_12_11_142151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,11 +84,6 @@ ActiveRecord::Schema.define(version: 2018_12_13_090941) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["regional_id"], name: "index_subdistricts_on_regional_id"
-  end
-
-  create_table "tests", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "user_investors", force: :cascade do |t|
